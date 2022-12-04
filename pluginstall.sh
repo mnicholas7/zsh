a0c0568a6031bf037bf73c8b66b5453f2d9bb430
Michael Nicholas <michael.nicholas@cloud5.com>
2022-12-04 01:55:15 -0500

    removed trailing w space

--- pluginstall.sh
+++ pluginstall.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+sudo apt install curl -y
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \                                  
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-                                                                                                      
-git clone https://github.com/zsh-users/zsh-autosuggestions \                                        
-    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions    
-  
+
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+    ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 git clone https://github.com/softmoth/zsh-vim-mode.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vim-mode
-  
+
 git clone https://github.com/romkatv/powerlevel10k.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
 wget https://github.com/gokcehan/lf/releases/download/r27/lf-linux-amd64.tar.gz
-gunzip lf-linux-amd64.tar.gz 
+gunzip lf-linux-amd64.tar.gz
 tar -xvf lf-linux-amd64.tar
 sudo mv lf /usr/local/bin/
 
