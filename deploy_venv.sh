c097795bfba8329061c5a089f00a62102c1492e8
IT Gains <itgains@Michaels-MacBook-Air.local>
2022-12-05 21:07:59 -0500

    tweaking deploy_venv.sh

--- deploy_venv.sh
+++ deploy_venv.sh
@@ -1,7 +1,7 @@
 # before 15.1.0
-# virtualenv --no-site-packages --distribute .env &&\
-#     source .env/bin/activate &&\
+# virtua.venv --no-site-packages --distribute .venv &&\
+#     source .venv/bin/activate &&\
 #     pip install -r requirements.txt
 
 # after deprecation of some arguments in 15.1.0
-virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+python -m .venv .venv && source .venv/bin/activate && pip install -r requirements.txt
