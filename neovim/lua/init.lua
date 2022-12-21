2b07ad65f0b288facbb375c28d8735e0302946e7
Michael Nicholas <michael.nicholas@cloud5.com>
2022-12-20 19:43:38 -0500

    louie lua

---
+++ neovim/lua/init.lua
@@ -0,0 +1,173 @@
+local set = vim.opt
+
+
+
+set.clipboard = 'unnamedplus'
+set.mouse = '' 
+set.hlsearch = true
+set.incsearch = true
+set.ruler = true
+set.showcmd = true
+set.expandtab = true
+set.shiftwidth = 2
+set.softtabstop = 2
+set.showcmd = true
+set.foldmethod = 'indent'
+-- set.foldcolumn = '1'
+set.laststatus = 2
+set.listchars = 'tab:†ⅱ,trail:Ѫ,eol:$,nbsp:ϟ'
+
+
+
+-- vim.opt.clipboard = 'unnamedplus'
+
+-- for name,_ in pairs(package.loaded) do  for name,_ in pairs(package.loaded) do
+-- function _G.ReloadConfig()
+--   for name,_ in pairs(package.loaded) do
+--     if name:match('^user') and not name:match('nvim-tree') then
+--       package.loaded[name] = nil
+--     end
+--   end
+-- 
+--   -- dotfile(vim.env.MYVIMRC)
+--   loadfile('init')
+--   vim.notify("Nvim configuration reloaded!", vim.log.levels.INFO)
+-- end
+
+
+vim.g.mapleader = " "
+
+-- " " Copy to clipboard
+
+-- vim.api.nvim_set_keymap( 'v', '<leader>y'  , '"+y'   , { vnoremap = true })               
+vim.api.nvim_set_keymap( 'n', '<leader>Y'  , '"+yg_' , { noremap = true })         
+vim.api.nvim_set_keymap( 'n', '<leader>y'  , '"+y'   , { noremap = true })         
+vim.api.nvim_set_keymap( 'n', '<leader>yy' , '"+yy'  , { noremap = true })         
+
+-- " " Paste from clipboard
+
+vim.api.nvim_set_keymap( 'n', '<leader>p', '"+p' , { noremap = true })
+vim.api.nvim_set_keymap( 'n', '<leader>P', '"+P' , { noremap = true })
+-- vim.api.nvim_set_keymap( 'v', '<leader>p', '"+p' , { vnoremap = true })
+-- vim.api.nvim_set_keymap( 'v', '<leader>P', '"+P' , { vnoremap = true })
+
+
+-- just a test really to see if this file is working still
+vim.api.nvim_set_keymap('i', 'jkl', '<ESC>', { noremap = true })
+
+vim.o.relativenumber = true
+
+-- require('statusline')
+--  use('eviline')
+
+-- vim.api.nvim_set_keymap("n", "<leader><CR>", "<cmd>lua ReloadConfig()<CR>", { noremap = true, silent = false })
+
+
+
+
+
+
+
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- My plugins here
+  use 'nvim-lualine/lualine.nvim'
+
+  -- use 'nvim-lua/lsp_extensions.nvim'
+  -- use 'ms-jpq/coq_nvim'
+
+  -- colorscheme
+  use 'joshdick/onedark.vim'
+
+  -- fuzzy finder
+  use 'junegunn/fzf'
+
+  use 'tpope/vim-surround'
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-unimpaired'
+  use 'xiyaowong/nvim-transparent'
+
+  -- Post-install/update hook with neovim command
+  -- use { 'nvim-treesitter/nvim-treesitter'}
+  --
+  --
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+  
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-nvim-lua'},
+  
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+
+set.signcolumn = 'yes'
+set.termguicolors = true
+pcall(vim.cmd, 'colorscheme onedark')
+
+require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
+
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.pyright.setup{}
+--
+-- 'mnlualine' is located here: ~/PUBREPOS/neovim/runtime/lua
+require('mnlualine')
+-- 'mnauto' is located here: ~/PUBREPOS/neovim/runtime/lua
+require('mnauto')
+-- require('coq')
+--
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+lsp.nvim_workspace()
+lsp.setup()
+
+end)
+
+
---
+++ neovim/lua/mnauto.lua
@@ -0,0 +1,87 @@
+local api = vim.api
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yankGrp,
+})
+
+
+-- lua exec file
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "lua" }, command = [[noremap  <buffer><F3>      :w<CR>:exec '!lua -i' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "lua" }, command = [[inoremap <buffer><F3> <esc>:w<CR>:exec '!lua -i' shellescape(@%, 1)<CR>]] }
+)
+--
+--
+-- perl exec file
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "perl" }, command = [[noremap  <buffer><F4>      :w<CR>:exec '!perl -d' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "perl" }, command = [[inoremap <buffer><F4> <esc>:w<CR>:exec '!perl -d' shellescape(@%, 1)<CR>]] }
+)
+--
+-- zsh exec file
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "zsh" }, command = [[noremap  <buffer><F5>      :w<CR>:exec '!zsh -x -o shwordsplit' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "zsh" }, command = [[inoremap <buffer><F5> <esc>:w<CR>:exec '!zsh -x -o shwordsplit' shellescape(@%, 1)<CR>]] }
+)
+-- bash exec file 
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "bash" }, command = [[noremap  <buffer><F6>        :w<CR>:exec 'terminal bash -x' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "bash" }, command = [[inoremap <buffer><F6>   <esc>:w<CR>:exec 'terminal bash -x' shellescape(@%, 1)<CR>]] }
+)
+-- python exec file 
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[noremap  <buffer><F7>      :w<CR>:exec 'terminal python3' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[inoremap <buffer><F7> <esc>:w<CR>:exec 'terminal python3' shellescape(@%, 1)<CR>]] }
+)
+
+-- python -m pdb
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[noremap  <buffer><F8>      :w<CR>:exec 'terminal python3 -m pdb' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[inoremap <buffer><F8> <esc>:w<CR>:exec 'terminal python3 -m pdb' shellescape(@%, 1)<CR>]] }
+)
+
+-- python interact
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[noremap  <buffer><F8>      :w<CR>:exec 'terminal python3 -i' shellescape(@%, 1)<CR>]] }
+)
+
+api.nvim_create_autocmd(
+  "FileType",
+  { pattern = { "python" }, command = [[inoremap <buffer><F8> <esc>:w<CR>:exec 'terminal python3 -i' shellescape(@%, 1)<CR>]] }
+)
+
+
